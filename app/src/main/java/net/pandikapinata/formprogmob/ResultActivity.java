b98d2fc5de5726258417a3ad1fba8170b316dedd
pandikapinata <pandikapinata27@gmail.com>
2018-03-20 12:03:45 +0800

    Add new Activity

--- app/src/main/java/net/pandikapinata/formprogmob/ResultActivity.java
+++ app/src/main/java/net/pandikapinata/formprogmob/ResultActivity.java
@@ -1,8 +1,10 @@
 package net.pandikapinata.formprogmob;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Pandika on 3/20/2018.
  */
 
-public class ResultActivity {
+public class ResultActivity extends AppCompatActivity {
 }
